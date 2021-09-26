cc8e55ba363886c15841f295c81f8eb8aba23285
Alex norman <mange.drawing.123@gmail.com>
2021-09-26 18:14:21 +0700

    kinda work with three tab or above

--- balloon.js
+++ balloon.js
@@ -25,12 +25,13 @@ function button() {
 
   let regexAction = /^(?!\t|^([A-Z\s]+)$)(\S.*)/gm;
   let regexPanel = /^(PANEL\s+\S+)(.*?)(?=PANEL|$(?!\n))/gms;
+  let regexPanelEnter = /((\n{3,})|^(PANEL\s+(\S+))|((NORMAL|FULL|LEFT|RIGHT|FOCUS|CENTER|NONE)))(.*?)(?=\n\n\n\n?|^(PANEL\s+\S+)|^(NORMAL|FULL|LEFT|RIGHT|FOCUS|CENTER|NONE)|\Z)/gms
   let regexNameAndBalloon = /^(\t\t)(.*?)(?=\t\t|^(?!\t|$).*)/gms;
   let regexName = /(\t\t)(\S.*)/gm;
   let regexBalloon = /(^\t)(\S.*)/gm;
 
   result = s.replace(regexAction, '<div class="action">$2</div>');
-  result = result.replace(regexPanel, '<div class="$1">$2</div>');
+  result = result.replace(regexPanelEnter, '<div class="PANEL $4$6">\n$7\n</div>\n');
   result = result.replace(regexNameAndBalloon, '<div class="nameAndBalloon">$1$2</div>');
   result = result.replace(regexName, '<div class="name">$2</div>');
   result = result.replace(regexBalloon, '<div class="balloon">$2</div>');
