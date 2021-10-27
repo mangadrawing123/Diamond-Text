386bb811cde2409e4b9865406b92d96cd27c7750
Alex norman <mange.drawing.123@gmail.com>
2021-10-27 21:55:39 +0700

    img white relatvie path

--- src/textareaFontsizeIncrease.js
+++ src/textareaFontsizeIncrease.js
@@ -1,12 +1,12 @@
 
 $("#decreaseText").click(function() {
-    curSize = parseInt($("textarea").css("font-size")) - 4;
+    curSize = parseInt($("textarea").css("font-size")) - 3;
 	// if (curSize >= 14) 
     $("textarea").css("font-size", curSize);
     // alert("hello");
 });
 $("#increaseText").click(function() {
-    curSize = parseInt($("textarea").css("font-size")) + 4;
+    curSize = parseInt($("textarea").css("font-size")) + 3;
 	// if (curSize >= 14) 
     $("textarea").css("font-size", curSize);
     // alert("hello");
