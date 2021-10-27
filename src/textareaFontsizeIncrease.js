e0dd42fb4cf1b96c1e19804f9bc5f644a1b8bda0
Alex norman <mange.drawing.123@gmail.com>
2021-10-27 13:47:12 +0700

    textarea button anddownlado 2000px

--- src/textareaFontsizeIncrease.js
+++ src/textareaFontsizeIncrease.js
@@ -1,12 +1,12 @@
 
 $("#decreaseText").click(function() {
-    curSize = parseInt($("textarea").css("font-size")) - 2;
+    curSize = parseInt($("textarea").css("font-size")) - 4;
 	// if (curSize >= 14) 
     $("textarea").css("font-size", curSize);
     // alert("hello");
 });
 $("#increaseText").click(function() {
-    curSize = parseInt($("textarea").css("font-size")) + 2;
+    curSize = parseInt($("textarea").css("font-size")) + 4;
 	// if (curSize >= 14) 
     $("textarea").css("font-size", curSize);
     // alert("hello");
